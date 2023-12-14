aaf6c0136bf1ae8c67de7703d51777c7365cd044
Gabriel Farias <imgabrielsf@gmail.com>
2023-12-14 08:28:19 -0300

    Order module

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
+import CustomersRepository from '@modules/customers/typeorm/repositories/CustomersRepository';
+import { ProductRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowOrderService {
+  public async execute({ id }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+
+    const order = await ordersRepository.findById(id);
+
+    if (!order) {
+      throw new AppError('Order not found.');
+    }
+
+    return order;
+  }
+}
+
+export default ShowOrderService;
